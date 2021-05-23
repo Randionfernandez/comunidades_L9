8378cef62fd8fa79c5ca30a40d9984e34cf8f2ec
Rafael Andión <randion@cifpfbmoll.eu>
2021-05-23 03:39:09 +0200

    Finalizada mezcla con navbar. Revisar, especialmente bootstrap

--- resources/views/comunidades/index.blade.php
+++ resources/views/comunidades/index.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <x-app-layout>
 
     @if(session('status'))
@@ -20,14 +19,13 @@
      <div class="col-12 col-sm-10 col-lg-6 mx-auto">
     <button class="btn btn-primary btn-lg btn-block"> @lang('Delete')</button>
     </div>
-    -->
+
     @if ($user->comunidades->count() > 0)
         <table class="table table-responsive">
             <thead>
                 <tr>
-                    <th>@lang('id')</th>
-                    <th>@lang('denom')</th>
                     <th>@lang('cif')</th>
+                    <th>@lang('denom')</th>
                     <th>@lang('president')</th>
                     <th>@lang('secretary')</th>
                     <th>@lang('responsable')</th>
@@ -37,9 +35,8 @@
             <tbody>
 
                 <tr>
-                    <td>{{$comunidad->id}}</td>
-                    <td>{{$comunidad->denom}}</td>
                     <td>{{$comunidad->cif}}</td>
+                    <td>{{$comunidad->denom}}</td>
                     <td>{{$comunidad->president}}</td>
                     <td>{{$comunidad->secretary}}</td>
                     <td>{{$comunidad->responsable}}</td>
@@ -55,49 +52,5 @@
     @else
         <h1>@lang('There are not communities created yet')</h1>
     @endif
+        -->
 </x-app-layout>
-=======
-@extends('layouts.layout')
-
-<?php $title= 'Comunidades'; ?>
-@section('title', $title )
-
-
-@section('content')
-
-<div class="row">
-    <h1>@lang('Comunidades')</h1>
-    <a class="col-auto btn btn-primary mb-1"
-       href="{{ route('comunidades.create') }}"
-       >@lang('Crear comunidad')</a>
-</div>
-<table class="table table-striped table-bordered">
-    <thead>
-        <tr>
-            <th>@lang('CIF')</th>
-            <th>@lang('CP')</th>
-            <th>@lang('Denominación')</th>
-            <th>@lang('Acción')</th>
-        </tr>
-    </thead>
-    <tbody>
-
-        @foreach($comunidades as $comunidad)
-        <tr>
-            <td>{{ $comunidad->cif }}</td>
-            <td>{{ $comunidad->cp }}</td>
-            <td>{{ $comunidad->denom }}</td>
-            <td class="text-center">
-                <!-- show  -->
-                <a class="btn btn-small btn-success" href="{{ route('comunidades.show', $comunidad) }}">@lang('Mostrar')</a>
-            </td>
-        </tr>
-        @endforeach
-
-    </tbody>
-</table>
-
-{{-- $comunidades->links() --}}
-
-@endsection
->>>>>>> master
