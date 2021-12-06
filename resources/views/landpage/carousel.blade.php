f08519eaaa3e1b10a2d7e11ca9a1d9e0ee06415e
Rafael Andión <randion@cifpfbmoll.eu>
2021-12-06 06:39:00 +0100

    Corregida landpage de Inés

---
+++ resources/views/landpage/carousel.blade.php
@@ -0,0 +1,36 @@
+<div class = "container-fluid h-auto p-10">
+    <div class = "row mr-0 ">
+        <div class = "col-lg-12 pr-0">
+            <div id = "carousel" class = "carousel slide carousel-fade" data-bs-ride = "carousel">
+                <div class = "carousel-inner">
+                    <div class = "carousel-item active" data-bs-interval = "10000">
+                        <img class = "d-block w-100 " src = "images/portada.jpg" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "/images/villa.png" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "/images/piscina.jpg" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "images/fachada.jpg" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "images/parque.jpg" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "images/img1.jpg" alt = "Tu Vecindad">
+                    </div>
+                    <div class = "carousel-item" data-bs-interval = "10000">
+                        <img class = "d-block w-100" src = "images/gimnasio.jpg" alt = "Tu Vecindad">
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h4 class = "carousel-caption">Gestiona tu comunidad tranquilamente desde cualquier dispositivo móvil, tablet o pc </h4>
+            </div>
+
+        </div>
+
+    </div>
+</div>
